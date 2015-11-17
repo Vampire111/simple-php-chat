da52ea8e5bfabcd8687500e2377cc2609c2ec74c
MoshkovMS <soresmail@gmail.com>
2015-11-18 00:13:02 +0200

    Реєстрація
    даний модуль відповідає за реєстрацію нових користувачів у системі

---
+++ registration.php
@@ -0,0 +1,25 @@
+<html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<title>Регистрация</title>
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+  <form method="post" action="db.php" class="login">
+    <p>
+      <label for="login">Логин:</label>
+      <input type="text" name="login" id="login" value="">
+    </p>
+    <p>
+      <label for="password">Пароль:</label>
+      <input type="password" name="password" id="password" value="">
+    </p>
+    <p class="login-submit">
+      <button type="submit" class="login-button">Войти</button>
+    </p>
+  </form>
+</body>
+</html>
+
